1208069bbbff79f715cbca28cfd33dc8086e5ce7
Atsushi Yamamoto <yamaatsushi927@gmail.com>
2017-10-13 05:13:01 -0700

    Add slach command answers

--- app.rb
+++ app.rb
@@ -5,8 +5,27 @@ get '/' do
 end
 
 post '/' do
-  puts request.to_s
-  puts params
-  status 200
-  'This message is returned'
+  text = params.fetch('text').strip
+
+  case text
+  when 'ls'
+    puts 'Work:
+    Web Develper @ Ubysser (Oct 2017 ~ Present)
+    Student Developer Intern @ BlackBerry (Jan 2017 - Aug 2017)
+    Software Engineering Intern @ Prynt (Jul 2016 - Dec 2016)
+
+    Speaking:
+    StartupStorm workshops
+    DevTeach Montreal
+    OttawaJS
+
+    School:
+    University of British Columbia'
+  when 'jokes'
+    "Why did chicken cross the road?"
+  when 'ping'
+    "Hi!"
+  when 'origin'
+    "I'm originally from Kyoto, Japan. Grew 6 years in U.S, and now studies in Vancouver, Canada :flag-ca:"
+  end
 end
